42dbc0e56b4b47bf106d2a7ac3ca5fb9ddf30a2b
damian1898 <damiane.gianni@gmail.com>
2016-04-26 17:41:09 -0300

    Avance fechas json

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,16 +1,29 @@
 /* aftersave: uglifyjs <self> -mco ../../dist/js/functions.min.js */
 
 $(document).ready(function(){
+
+	$.ajaxSetup({
+		async: false
+	});
+
 	getInfo('json','matches');
 	getInfo('xml', 'posiciones');
 	getInfo('xml', 'fixture');
 	getInfo('xml', 'goleadores');
+
+	findFechas();
+	loadFecha();
+
+
 });
 
-var matches;
+var nextMatches;
 var positions = [];
 var fixture;
 var scorers;
+var fechaActual = [];
+var fechaProxima = [];
+var fechas = [];
 
 function getData(typeInfo,url,pos) {
 
@@ -18,13 +31,13 @@ function getData(typeInfo,url,pos) {
 		var xml = data;
 
 		if(typeInfo == 'posiciones')
-			positions[pos] = xmlToJson(xml);
+			positions[pos] = xmlToJson(xml).posiciones;
 
 		if(typeInfo == 'fixture')
-			fixture = xmlToJson(xml);
+			fixture = xmlToJson(xml).fixture;
 
 		if(typeInfo == 'goleadores')
-			scorers = xmlToJson(xml);
+			scorers = xmlToJson(xml).goleadores;
 	});
 }
 
@@ -32,7 +45,7 @@ function getInfo(typeF,typeInfo) {
 
 	if(typeF == 'json' && typeInfo == 'matches') {
 		$.getJSON( "http://data.bolavip.com/json/argentina.json", function( data ) {
-			matches = data;	
+			nextMatches = data;	
 		});
 	}
 
@@ -91,9 +104,70 @@ function getInfo(typeF,typeInfo) {
 	return obj;
 }
 
-function createMatches() {
-	$.each(fixture.fixture, function( index, value ) {	  
-		//http://data.bolavip.com/img/teams/31.gif
+function findFechas() {
+	var nameTournament = fixture.campeonato.text;
+	$(".site-tournament-title").html(nameTournament);
+	
+	$.each(fixture.fecha, function( index, matches ) {
+		if(matches.estado == 'actual') {
+			fechaActual[fechaActual.length] = matches;
+		}
+
+		if(matches.estado == 'proxima') {
+			fechaProxima[fechaProxima.length] = matches;
+			//fechaActual = matches;
+		}
+
+		fechas[fechas.length] = matches;
 	});	
 }
 
+function loadFecha() {
+	var sectionMatches = $(".site-tournament-matches");
+	sectionMatches.empty();
+	for (var i = fechaActual.length - 1; i >= 0; i--) {
+		if(fechaActual[i].orden != 3 || fechaActual[i].fn == 12) {
+			$.each(fechaActual[i].partido, function(index, partido){
+				cargarPartido(fechaActual[i],partido, sectionMatches);
+			});
+		} else {
+			cargarPartido(fechaActual[i],fechaActual[i].partido, sectionMatches);
+		}
+	}
+}
+
+function cargarPartido(fecha, partido, section) {
+	var divMatch = $("<div class='site-tournament-match'>");
+	var figure = $("<figure>");
+	var divTeamL = $("<div class='site-match-team'>");
+	var divTeamV = $("<div class='site-match-team'>");
+	var escudoLocal = $("<img class='site-match-team-escudo' src='http://data.bolavip.com/img/teams/"+partido.local.id+".gif' />");
+	var labelLocal = $("<label class='site-match-team-name'>"+partido.local.text+"</label>");
+
+	var labelGLocal = $("<label class='site-match-team-goles'>"+partido.goleslocal.text+"</label>");
+	var labelGVisitante = $("<label class='site-match-team-goles fLeft'>"+partido.golesvisitante.text+"</label>");
+
+	var escudoVisitante = $("<img class='site-match-team-escudo' src='http://data.bolavip.com/img/teams/"+partido.visitante.id+".gif' />");
+	var labelVisitante = $("<label class='site-match-team-name'>"+partido.visitante.text+"</label>");
+	
+
+	divTeamL.append(escudoLocal,labelLocal,labelGLocal);
+	divTeamV.append(escudoVisitante,labelVisitante,labelGVisitante);
+
+	figure.append(divTeamL,divTeamV);
+
+	divMatch.append(figure);
+
+	section.append(divMatch);
+
+}
+
+
+function findFecha (nbrFecha) {
+	fechaActual = [];
+	$.each(fechas, function(index,fecha) {
+		if(fecha.fn == nbrFecha) {
+			fechaActual.push(fecha);
+		}
+	});
+}
